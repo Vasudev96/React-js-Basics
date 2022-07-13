7d52fe8b32bd7a15a3c5bb34acbd276bc8d7f0c4
Vasudev <vasudev@avdevs.com>
2022-07-13 16:19:05 +0530

    Updates

---
+++ precache-manifest.905b9af470f9413641a3a048612147af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b5d35bfbaac8f0b7453",
+    "url": "/React-js-Basics/static/css/main.795b6972.chunk.css"
+  },
+  {
+    "revision": "6b5d35bfbaac8f0b7453",
+    "url": "/React-js-Basics/static/js/main.6b5d35bf.chunk.js"
+  },
+  {
+    "revision": "23172ffde8cf25f62d55",
+    "url": "/React-js-Basics/static/js/1.23172ffd.chunk.js"
+  },
+  {
+    "revision": "76046b597c722608ab86",
+    "url": "/React-js-Basics/static/css/2.7f2f8730.chunk.css"
+  },
+  {
+    "revision": "76046b597c722608ab86",
+    "url": "/React-js-Basics/static/js/2.76046b59.chunk.js"
+  },
+  {
+    "revision": "83fba0ddc73ad169e7ed",
+    "url": "/React-js-Basics/static/js/runtime~main.83fba0dd.js"
+  },
+  {
+    "revision": "4ed503ad21863a248eb8a79b747f2f22",
+    "url": "/React-js-Basics/index.html"
+  }
+];
